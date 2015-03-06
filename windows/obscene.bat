f88c70b31ea8b3e9e163f5c0a086d4448c693a9b
unknown <dehlen@LWMT-CL10ZW1.united.domain>
2015-03-06 15:57:10 +0100

    OB-29 changed bat file to run without errors and implemented first logic

--- windows/obscene.bat
+++ windows/obscene.bat
@@ -1,32 +1,33 @@
 @echo off
+
 set argC=0
 for %%x in (%*) do Set /A argC+=1
 
-if %argC% equ 1 (
-    goto install
-) else if %argC% equ 2 (
-    goto dir
-) else if %argC% equ 3 (
-    goto plugin
-)
-else (
-	echo "Unsupported arguments" 
-	echo .
-	echo "Usage:%0 install"
-	echo .
-	echo "%0 create directoryName"
-	echo .
-	echo "%0 plugin add pluginName"
-	echo .
-	echo "%0 plugin remove pluginName"
-	pause
-)
+if %argC% EQU 1 goto install
+if %argC% EQU 2 goto dir
+if %argC% EQU 3 goto plugin
+
+echo.
+echo "Unsupported arguments" 
+echo "Usage:%0 install"
+echo "%0 create directoryName"
+echo "%0 plugin add pluginName"
+echo "%0 plugin remove pluginName"
+echo.
+echo.
+goto :end
 
 :install
 echo "installing..."
+goto end
+
 :dir
 echo "creating directory..."
+goto end
+
 :plugin
 echo "adding/removing plugin..."
+goto end
 
+:end
 pause
